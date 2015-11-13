3c1314936782fc7b9083bf40f4e6ed0e5e62c431
kfiroo <kfirgolan@gmail.com>
2015-11-13 16:11:39 +0200

    respawner

---
+++ Assets/Scripts/Respawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawner : MonoBehaviour {
+
+	public float deathY;
+	public float respwanDrop;
+
+	private float initialX;
+	private float initialY;
+
+	// Use this for initialization
+	void Start () {
+		initialX = transform.position.x;
+		initialY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < deathY) {
+			transform.position = new Vector3(initialX, initialY + respwanDrop, transform.position.z);
+		}
+	}
+}
